1b15da952ce7bd0fbdadbdda4709248b730e8993
MelnikovAnton1511 <pidorpidorasov72@gmail.com>
2024-12-08 20:45:14 +0300

    Delete backend directory

--- backend/backend/olympiads/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import User
-
-@admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    list_display = ('email', 'is_active', 'is_admin')
-    list_filter = ('is_active', 'is_admin')
-    search_fields = ('email',)
-    ordering = ('email',)
--- backend/backend/olympiads/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import RegisterView, ConfirmEmailView
-
-urlpatterns = [
-    path('register/', RegisterView.as_view(), name='register'),
-    path('confirm-email/<str:token>/', ConfirmEmailView.as_view(), name='confirm-email'),
-]
