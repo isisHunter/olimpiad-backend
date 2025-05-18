11502e63abface324f257dc3d470312d0d9e4108
MelnikovAnton1511 <pidorpidorasov72@gmail.com>
2025-05-18 22:54:23 +0300

    commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import Search from './pages/Search'
 import Dashboard from './pages/Dashboard';
 import Error404 from './pages/404';
 import ResetPassword from './pages/ResetPassword';
+import AI from './pages/AIprep';
 import { useAuth } from './authcontext';
 import { Routes, Route, Navigate } from "react-router-dom";
 
@@ -19,6 +20,7 @@ const App = () => {
         <Route path="/confirm-email/:token" element={<ConfirmEmail />} />
         <Route path="/dashboard" element={user ? <Dashboard /> : <Navigate to="/enter" />} />
         <Route path="/reset-password" element={!user ? <ResetPassword /> : <Navigate to="/dashboard" />} />
+        <Route path="/AI" element={<AI />} />
         <Route path="*" element={<Error404 />} />
       </Routes>
   );
